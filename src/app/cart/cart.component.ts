221f2fcd0e179a87612c8121717ba0e206873e17
Solid-Lizard <95529607+Solid-Lizard@users.noreply.github.com>
2022-12-15 09:05:59 +0100

    Creación del formulario completa

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,6 +10,20 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
   constructor(
+    private formBuilder: FormBuilder,
     private cartService: CartService
   ) { }
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
 }
